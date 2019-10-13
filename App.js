e001f325d5992636c7d9dcfd165bcf62c873af7d
yasmin-alharasis <yasmin.jaalharasis@gmail.com>
2019-10-13 10:19:08 +0300

    finish the assment

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 // Q2: App Component (there are 7 mistakes)
 
 // WRITE || EDIT THE CODE UNDER THIS LINE
+import React ,{Component} from 'react';
 import Tasks from './components/Tasks';
 
 export default class App extends Component {
@@ -8,19 +9,29 @@ export default class App extends Component {
     title: 'ELIZABETH GREENE',
     todos: ['eat', 'eat eat', 'eat again']
   };
-  changeTitle() {
+  // changeTitle() {
+  //   // WRITE || EDIT THE CODE UNDER THIS LINE
+  //   state.title = 'AGGREGOR ZOLDYCK'
+  // }
+  changeTitle = ()=> {
     // WRITE || EDIT THE CODE UNDER THIS LINE
-    state.title = 'AGGREGOR ZOLDYCK'
+     
+    this.setState({title ='AGGREGOR ZOLDYCK'})
+    //state.title = 'AGGREGOR ZOLDYCK'
   }
+  
+ 
   render() {
     return (
+      <div>
       {/* WRITE || EDIT THE CODE UNDER THIS LINE */}
       {/* WRITE || EDIT THE CODE UNDER THIS LINE */}
-      <h1>App Component: state.title</h1>
+      <h1>App Component: {this.state.title}</h1>
       {/* WRITE || EDIT THE CODE UNDER THIS LINE */}
-      <button onClick={changeTitle}>Change Title</button>
+      <button onClick={this.changeTitle}>Change Title</button>
       {/* WRITE || EDIT THE CODE UNDER THIS LINE*2 */}
-      <Tasks tasks={todos} changeTitleFromChild={changeTitle} />
+      <Tasks tasks={this.state.todos} changeTitleFromChild={this.changeTitle} />
+      </div>
     )
   }
 }
