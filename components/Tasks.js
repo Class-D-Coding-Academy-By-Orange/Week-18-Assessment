c83109f4f050d23ddaa434ae9ef5f7c537b04ba5
NadaEidah <nada.eadah@gmail.com>
2019-10-13 10:14:10 +0300

    finehed assessment

--- components/Tasks.js
+++ components/Tasks.js
@@ -1,31 +1,40 @@
 // Q3: Tasks Component (there are 8 mistakes)
 
 import React, { Component } from 'react';
-// WRITE || EDIT THE CODE UNDER THIS LINE
-import Tasks from './Item';
+// WRITE || EDIT THE CODE UNDER THIS LINE*
+import Item from './Item';
 
-// WRITE || EDIT THE CODE UNDER THIS LINE
-class Tasks extends Component {
+// WRITE || EDIT THE CODE UNDER THIS LINE*
+export default class Tasks extends Component {
   state = {
     day: "Sat"
   };
   changeDay() {
     // WRITE || EDIT THE CODE UNDER THIS LINE
-    day = 'Sun'
+    
+    // day = 'Sun'
+
+    let newDay = this.state.day 
+      return newDay = 'Sun'
+      this.setState(day)
   }
   render() {
     return (
       <div>
-        {/* WRITE || EDIT THE CODE UNDER THIS LINE */}
-        <h1>Tasks Component: state.day</h1>
-        {/* WRITE || EDIT THE CODE UNDER THIS LINE */}
-        <button onClick={changeDay}>Change Tasks State</button>
+        {/* WRITE || EDIT THE CODE UNDER THIS LINE **/}
+        <h1>Tasks Component: {this.state.day}</h1>
+        {/* WRITE || EDIT THE CODE UNDER THIS LINE **/}
+        <button onClick={this.changeDay}>Change Tasks State</button>
         {/* WRITE || EDIT THE CODE UNDER THIS LINE*/}
-        <button onClick={changeTitle}>Change App State</button>
-        {/* WRITE || EDIT THE CODE UNDER THIS LINE *2 */}
-        todos.map((elem)=>{
-          return <Item e={elem}>
-        })
+        <button onClick={changeTitleFromChild}>Change App State</button>
+        {/* WRITE || EDIT THE CODE UNDER THIS LINE *2 **/}
+
+        {
+          todos.props.map((elem,i)=>{
+            return <Item e={elem} changeTitleFromChild={this.props.changeTitle}/>
+          })
+        }
+        
       </div>
     );
   }
