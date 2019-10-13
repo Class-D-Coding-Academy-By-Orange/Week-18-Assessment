5d66dd557127f7fa8725c0ff3cabe7c968dd8ec1
Raghadaladwan <raghadaledwan97@gmail.com>
2019-10-13 10:03:23 +0300

    finish the assessment

--- components/Item.js
+++ components/Item.js
@@ -1,13 +1,13 @@
 // Q4: Item Component (there are 3 mistakes)
 import React, { Component } from 'react';
 {/* WRITE || EDIT THE CODE UNDER THIS LINE */}
-class Item extends Component {
+export default class Item extends Component {
   render() {
     return (
       <div>
         <h1>Item Component:</h1>
         {/* WRITE || EDIT THE CODE UNDER THIS LINE * 2 */}
-        <p>elem</p>
+        <p>elem: {this.props.e}</p>
       </div>
     );
   }
